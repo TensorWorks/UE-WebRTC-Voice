3360a24a554d948fa70a3121caa92074dffffc68
Luke Bermingham <1215582+lukehb@users.noreply.github.com>
2021-05-09 21:40:18 +1000

    Added some notes about AudioSinks for how to proceed next.

--- BidiGame/Plugins/WebRTCAudio/Source/WebRTCAudio/Private/WebRTCInstance.cpp
+++ BidiGame/Plugins/WebRTCAudio/Source/WebRTCAudio/Private/WebRTCInstance.cpp
@@ -8,11 +8,15 @@ FWebRTCInstance::FWebRTCInstance()
 {
     //ToDo: Create PeerConnectionFactory etc to start WebRTC in UE.
 
+	// For each received audio track, set a sink on that audio track
+	// The audio sink can output raw audio data to UE or wherever
+	// See AudioTrackSinkInterface OnData()
+
     this->PeerConnectionFactory = webrtc::CreatePeerConnectionFactory(
 		nullptr, // network_thread
 		nullptr, // worker_thread
 		nullptr, // signal_thread
-		new rtc::RefCountedObject<FAudioCapturer>(), // audio device manager
+		nullptr, // audio device module, could be like as FAudioCapturer or FAudioRenderer
 		webrtc::CreateAudioEncoderFactory<webrtc::AudioEncoderOpus>(),
 		webrtc::CreateAudioDecoderFactory<webrtc::AudioDecoderOpus>(),
 		nullptr, // video encoder factory
